ab79aace5d2bfc0cd807d7adfa7293fd60efd093
Ruiyu Wu <ruiyuwu1998@gmail.com>
2019-12-02 16:47:56 -0500

    Update 0322.js

--- 0322.js
+++ 0322.js
@@ -1,22 +1,20 @@
-var coinChange = function(coins, amount, memo = {}) {
-    const key = coins.concat(amount);
-    if (key in memo) return memo[key];    
+var coinChange = function(coins, amount, memo = {}, top=true) {
+    if (amount in memo) return memo[amount];    
 
     if (amount === 0) return 0;
     let sum = Infinity;
    
     for (let i = 0; i < coins.length; i++) {
-        coin = coins[i];
+        const coin = coins[i];
         let attempt = 0;
         if (coin <= amount) {
-            attempt += 1 + coinChange(coins, amount - coin, memo);
-            if (!attempt) continue;
+            attempt += 1 + coinChange(coins, amount - coin, memo, false);
             if (attempt < sum) {
                 sum = attempt;
             }
         }
     }
-    if (sum === Infinity) return -1;
-    memo[key] = sum;
+    if (top && sum === Infinity) return -1;
+    memo[amount] = sum;
     return sum;
 };
