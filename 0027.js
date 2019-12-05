2b7edae48ee65f257aa1316b929baa58ef4af1be
Ruiyu Wu <ruiyuwu1998@gmail.com>
2019-12-05 12:50:02 -0500

    0027

---
+++ 0027.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] === val) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return i;
+};
