407899271fc39849615c87e2eacd99d319236087
Ruiyu Wu <ruiyuwu1998@gmail.com>
2019-12-05 12:42:34 -0500

    0026

---
+++ 0026.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    let i = 1;
+    while (i < nums.length) {
+        if (nums[i] === nums[i - 1]) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return i;
+};
